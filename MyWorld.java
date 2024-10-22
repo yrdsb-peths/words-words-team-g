69b47d75fa7f12d6ecc1eb88a9ac9e043ea56e65
Jackyjackyjackyjackyjackyjackyjacky <349922815@gapps.yrdsb.ca>
2024-10-22 13:30:26 -0400

    idk

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,14 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+
     public MyWorld() {
         super(300, 1000, 1);
     }
+
+    public void makeWord() {
+        Label word = new Label("test", 14);
+    }
+
+
 }
