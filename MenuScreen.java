55f1836deb0dd1e3b341f186e71992d78894d587
MZhang-9 <m2volleyballa@gmail.com>
2024-10-22 12:37:41 -0400

    changed size so its more portrait

---
+++ MenuScreen.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MenuScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MenuScreen extends World
+{
+
+    /**
+     * Constructor for objects of class MenuScreen.
+     * 
+     */
+    public MenuScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(500, 700, 1); 
+    }
+}
