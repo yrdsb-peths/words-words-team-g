c6abb7dce208477e3d6c782d1f7f9e4fb1689254
MZhang-9 <m2volleyballa@gmail.com>
2024-10-23 13:29:52 -0400

    added character selection cycling between them

---
+++ CharacterDisplay.java
@@ -0,0 +1,12 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class CharacterDisplay extends Actor
+{
+    public CharacterDisplay(GreenfootImage initialImage) {
+        setImage(initialImage);
+    }
+    
+    public void setImage(GreenfootImage image) {
+        super.setImage(image);  // Update the actor's image
+    }
+}
--- CharacterSelection.java
+++ CharacterSelection.java
@@ -5,18 +5,25 @@ public class CharacterSelection extends World
     private World menuScreen;
     private int index = 0;
     private GreenfootImage[] characters;
-    
+    private CharacterDisplay characterDisplay; 
     public CharacterSelection()
     {    
+        
         super(500, 700, 1);
         setBackground(new GreenfootImage("Background.jpg"));
         
-        // Load character images into an array
-        characters = new GreenfootImage[] {
-            new GreenfootImage("Spaceship1.png"),
-            new GreenfootImage("Spaceship2.png"),
-            new GreenfootImage("Spaceship3.png")
-        };
+        GreenfootImage spaceShip1 = new GreenfootImage("Spaceship1.png");
+        spaceShip1.scale(180, 60);
+        
+        GreenfootImage spaceShip2 = new GreenfootImage("Spaceship2.png");
+        spaceShip2.scale(200, 150);
+        
+        GreenfootImage spaceShip3 = new GreenfootImage("Spaceship3.png");
+        spaceShip3.scale(100, 120);
+        
+        characters = new GreenfootImage[] { spaceShip1, spaceShip2, spaceShip3 };
+        characterDisplay = new CharacterDisplay(characters[index]);
+        addObject(characterDisplay, 250, 500);
         // Add Next and Previous buttons
         addObject(new Button(this::nextCharacter, "Next"), 250, 300);
         addObject(new Button(this::previousCharacter, "Previous"), 250, 350);
@@ -39,6 +46,6 @@ public class CharacterSelection extends World
         }
     }
     private void updateCharacterImage() {
-        setBackground(characters[index]); // Update the background with the current character image
+        characterDisplay.setImage(characters[index]);  // Set the image of the actor to the current character image
     }
 }
