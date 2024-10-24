e162f07c211be337bc370a772f15cb147556fd47
MZhang-9 <m2volleyballa@gmail.com>
2024-10-24 00:01:50 -0400

    changed spacing, added menuScreen instructions

--- MenuScreen.java
+++ MenuScreen.java
@@ -4,21 +4,36 @@ public class MenuScreen extends World
 {    
     //Menu music
     private GreenfootSound menuMusic;
+    private Label instructionLabel;
     
     //menu of buttons
     public MenuScreen()
     {    
+        
         //size of world
         super(500, 700, 1);
-        setBackground(new GreenfootImage("Background.jpg")); 
+        setBackground(new GreenfootImage("Background.jpg"));
+        
+        //setting music
         menuMusic = new GreenfootSound("MenuScreenMusic.mp3");  
+        
+        //adding buttons
         addObject(new Button(this::goInstructions, "Instructions"), 250, 360);
         addObject(new Button(this::goHighScores, "High Scores"), 250, 310); 
         addObject(new Button(this::goDifficulty, "Start Game"), 250, 260);
         addObject(new Button(this::goCharacterSelection, "Character selection"), 250, 410);
+        
+        //changing volume and allowing a loop play
         menuMusic.setVolume(0);
         menuMusic.playLoop();
         menuMusic.pause();
+        
+        //adding instructions
+        instructionLabel = new Label("please select a character to begin!", 30);
+        
+        // Add the instruction label to the screen
+        addObject(instructionLabel, 250, 200);
+
     }
     
     //going to intruction screen
