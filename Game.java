2357c3ea1d15ade781cdd256dc30dc9bc32d0d4c
Jackyjackyjackyjackyjackyjackyjacky <349922815@gapps.yrdsb.ca>
2024-10-24 12:25:35 -0400

    w key

--- Game.java
+++ Game.java
@@ -20,4 +20,17 @@ public class Game extends World
         MainShip userShip = new MainShip(2);
         addObject(userShip, 250, 600);
     }
+
+    public void makeWord() { // placeholder for making stuff fall down
+        Enemy enemy = new Enemy();
+        int  randomNum = Greenfoot.getRandomNumber(300);
+        enemy.setStartX(randomNum);
+        addObject(enemy, randomNum, 0);
+    }
+    public void act() { // press W key to make stuff fall down
+        if(Greenfoot.isKeyDown("W"))
+        {
+            makeWord();
+        }
+    }
 }
