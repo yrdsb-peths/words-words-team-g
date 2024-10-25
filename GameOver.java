6c0731c59f1b85530fb11b2c941f89a4b4c434dc
Charlie-006 <440028963@gapps.yrdsb.ca>
2024-10-25 12:46:18 -0400

    Created the input method
    It requests the user to input their name

--- GameOver.java
+++ GameOver.java
@@ -2,7 +2,9 @@ import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class GameOver extends World {
   private World menuScreen;
-  String ending = "GameOver";
+  private String ending = "GameOver";
+  private String userName = "";
+  private Label input;
 
   public GameOver(World menuScreen) {
     super(500, 700, 1);
@@ -15,9 +17,45 @@ public class GameOver extends World {
     addObject(new Button(this::goMenuScreen, "Menu"), 250, 300);
   }
 
+  public void act() {
+    requestName();
+  }
+
   public void DisplayGameOver() {
     Label end = new Label(ending, 40);
     addObject(end, 250, 100);
+
+    Label name = new Label("Enter your name: ", 40);
+    addObject(name, 250, 150);
+
+    input = new Label("", 40);
+    addObject(input, 250, 200);
+  }
+
+  public void requestName() {
+    String key = Greenfoot.getKey();
+    if (key != null) {
+      if (key.equals("enter")) {
+        // Display the final username
+        Label nameLabel = new Label("Player: " + userName, 30);
+        addObject(nameLabel, 250, 250); // Position to avoid overlap with other labels
+      } else if (key.equals("backspace")) {
+        if (userName.length() > 0) {
+          userName = userName.substring(0, userName.length() - 1);
+        }
+      } else if (key.length() == 1) {
+        // Check for shift and add character accordingly
+        if (Greenfoot.isKeyDown("shift")) {
+          key = key.toUpperCase();
+        }
+        userName += key;
+      }
+      input.setValue(userName);
+    }
+  }
+
+  public String getUserName() {
+    return userName;
   }
 
   public void goMenuScreen() {
