8915d46fd8c6e8afbc77d8fb6db1137757276fd3
HL231231 <341036663@gapps.yrdsb.ca>
2024-11-03 01:31:26 -0400

    Squashed commit of the following:
    commit b7214456eab92c971ac89c9f96f0cf567d176346
    Author: MZhang-9 <m2volleyballa@gmail.com>
    Date:   Sat Nov 2 10:24:32 2024 -0400
    
        Update Game.java
    
    commit 7b5c7468fd077a00b0cc23b2237c211660d3686c
    Merge: 2e9e462 b67e09f
    Author: MZhang-9 <m2volleyballa@gmail.com>
    Date:   Sat Nov 2 10:07:54 2024 -0400
    
        Merge pull request #47 from yrdsb-peths/ScoreSystem
    
        Added a scoring system(1 point per ship) added it to the game over pa…
    
    commit b67e09fc08efb343840810656522e8dd3297af3b
    Merge: 9b390ce 2e9e462
    Author: MZhang-9 <m2volleyballa@gmail.com>
    Date:   Sat Nov 2 10:07:47 2024 -0400
    
        Merge branch 'main' into ScoreSystem
    
    commit 2e9e4622428c16020f14058ff932d4a5aecab1f9
    Merge: 12f4ba4 1e0d543
    Author: Jackyjackyjackyjackyjackyjackyjacky <349922815@gapps.yrdsb.ca>
    Date:   Sat Nov 2 01:07:38 2024 -0400
    
        Merge pull request #48 from yrdsb-peths/explosion-ability
    
        Explosion ability
    
    commit 1e0d5430457302e125945427004208796d82f61c
    Author: Jackyjackyjackyjackyjackyjackyjacky <349922815@gapps.yrdsb.ca>
    Date:   Sat Nov 2 01:07:13 2024 -0400
    
        merge fixes
    
    commit 304683cd80806d84929ab900e4e29e1016b5675f
    Merge: a550047 12f4ba4
    Author: Jackyjackyjackyjackyjackyjackyjacky <349922815@gapps.yrdsb.ca>
    Date:   Sat Nov 2 00:42:19 2024 -0400
    
        Merge branch 'main' into explosion-ability
    
    commit a5500473ae18ddae3e266d09c9f0290cabc01e6a
    Author: Jackyjackyjackyjackyjackyjackyjacky <349922815@gapps.yrdsb.ca>
    Date:   Fri Nov 1 21:10:28 2024 -0400
    
        comments
    
    commit 718970b1ac85e72dd69885d964c8642344651c1a
    Author: Jackyjackyjackyjackyjackyjackyjacky <349922815@gapps.yrdsb.ca>
    Date:   Fri Nov 1 19:35:57 2024 -0400
    
        explosion works on multiple enemies
    
    commit 9b390ce446b84d4ae6d8d85e90c5e7dba00b462b
    Author: MZhang-9 <m2volleyballa@gmail.com>
    Date:   Fri Nov 1 19:14:46 2024 -0400
    
        Added a scoring system(1 point per ship) added it to the game over page as well
    
    commit 12f4ba44fdeba12abcf544486fde0ae41ae8f466
    Merge: e2da4ea aaf1b46
    Author: MZhang-9 <m2volleyballa@gmail.com>
    Date:   Fri Nov 1 14:17:19 2024 -0400
    
        Merge pull request #46 from yrdsb-peths/bugfix
    
        Fixed gameover screen transition bug
    
    commit aaf1b4644ee8476a239dd029d56707ebdb557e13
    Author: Charlie-006 <440028963@gapps.yrdsb.ca>
    Date:   Fri Nov 1 14:12:08 2024 -0400
    
        deleted
    
    commit a0810732d956dd7cd7a17bbcf40de2d8b92cd621
    Author: Charlie-006 <440028963@gapps.yrdsb.ca>
    Date:   Fri Nov 1 14:05:30 2024 -0400
    
        fixed gameover screen transition bug
    
    commit f76acf57a147f33d9d32a2702e5a2ca8b7c9532f
    Author: Jackyjackyjackyjackyjackyjackyjacky <349922815@gapps.yrdsb.ca>
    Date:   Fri Nov 1 13:30:35 2024 -0400
    
        errors
    
    commit da40d84dd9ec8387573e26f157c0934c4fe249b8
    Author: Jackyjackyjackyjackyjackyjackyjacky <349922815@gapps.yrdsb.ca>
    Date:   Thu Oct 31 23:29:22 2024 -0400
    
        aoe attack
    
    commit 2d25b512f16e49e3c4eea71e1e48cd9eb07d99e7
    Author: Jackyjackyjackyjackyjackyjackyjacky <349922815@gapps.yrdsb.ca>
    Date:   Thu Oct 31 21:07:51 2024 -0400
    
        cleaned up code
    
    commit 9613052dbb9797bae265e7bf2ee0efb3d1d9c49b
    Author: Jackyjackyjackyjackyjackyjackyjacky <349922815@gapps.yrdsb.ca>
    Date:   Thu Oct 31 20:04:25 2024 -0400
    
        used scanner to read text
    
    commit 053c97105a9d6b5fcf38a52089a9399ae934c4cc
    Author: Jackyjackyjackyjackyjackyjackyjacky <349922815@gapps.yrdsb.ca>
    Date:   Thu Oct 31 12:59:49 2024 -0400
    
        redo

--- GameOver.java
+++ GameOver.java
@@ -2,75 +2,72 @@ import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.util.*;
 
 public class GameOver extends World {
-  public static ArrayList<NameScore> UserNames = new ArrayList<>();
-  private World menuScreen;
-  private String ending = "GameOver";
-  private String userName = "";
-  private Label input;
+    public static ArrayList<NameScore> UserNames = new ArrayList<>();
+    private MenuScreen menuScreen;
+    private String ending = "GameOver";
+    private String userName = "";
+    private Label input;
+    private boolean inputAccepted = true;
+    private int finalScore;
 
+    public GameOver(int finalScore) {
+        super(500, 700, 1);
+        this.finalScore = finalScore;
+        menuScreen = new MenuScreen();
+        setBackground(new GreenfootImage("Background.jpg"));
+        DisplayGameOver();
 
-  private boolean inputAccepted = true;
-
-  public GameOver(World menuScreen) {
-    super(500, 700, 1);
-    setBackground(new GreenfootImage("Background.jpg"));
-
-    this.menuScreen = menuScreen;
-
-    DisplayGameOver();
-
-    addObject(new Button(this::goMenuScreen, "Menu"), 250, 600);
-  }
-
-  public void act() {
-    requestName();
-  }
-
-  public void DisplayGameOver() {
-    Label end = new Label(ending, 40);
-    addObject(end, 250, 100);
-
-    Label name = new Label("Enter your name: ", 40);
-    addObject(name, 250, 150);
-
-    input = new Label("", 40);
-    addObject(input, 250, 200);
-  }
-
-  public void requestName() {
-    if (!inputAccepted) return;
-
-    String key = Greenfoot.getKey();
-    if (key != null) {
-      if (key.equals("enter")) {
-        Random ran = new Random();
-        int testValue = ran.nextInt(10);
-        NameScore playerinfo = new NameScore(userName, testValue);
-        UserNames.add(playerinfo);
-
-        Label nameLabel = new Label("Player: " + userName, 40);
-        Label nameLabel2 = new Label("Score: " + playerinfo.getScores(), 40);
-
-        addObject(nameLabel, 250, 250);
-        addObject(nameLabel2, 250, 300);
-
-        inputAccepted = false;
-      } else if (key.equals("backspace")) {
-        if (userName.length() > 0) {
-          userName = userName.substring(0, userName.length() - 1);
-        }
-      } else if (key.length() == 1) {
-        // Check for shift and add character accordingly
-        if (Greenfoot.isKeyDown("shift")) {
-          key = key.toUpperCase();
-        }
-        userName += key;
-      }
-      input.setValue(userName);
+        addObject(new Button(this::goMenuScreen, "Menu"), 250, 600);
+        
+        Label scoreDisplay = new Label("Final Score: " + finalScore, 40); // Display final score
+        addObject(scoreDisplay, 250, 290);
     }
-  }
 
-  public void goMenuScreen() {
-    Greenfoot.setWorld(menuScreen);
-  }
+    public void act() {
+        requestName();
+    }
+
+    public void DisplayGameOver() {
+        Label end = new Label(ending, 40);
+        addObject(end, 250, 100);
+
+        Label name = new Label("Enter your name: ", 40);
+        addObject(name, 250, 150);
+
+        input = new Label("", 40);
+        addObject(input, 250, 200);
+    }
+
+    public void requestName() {
+        if (!inputAccepted) return;
+
+        String key = Greenfoot.getKey();
+        if (key != null) {
+            if (key.equals("enter") && !userName.trim().isEmpty()) {
+                NameScore playerInfo = new NameScore(userName, finalScore); // Use final score
+                UserNames.add(playerInfo);
+
+                // Displaying player and score labels in the GameOver screen
+                Label nameLabel = new Label("Player: " + userName, 40);
+
+                addObject(nameLabel, 250, 250);
+                inputAccepted = false;
+            } else if (key.equals("backspace")) {
+                if (userName.length() > 0) {
+                    userName = userName.substring(0, userName.length() - 1);
+                }
+            } else if (key.length() == 1 && userName.length() < 20) {  // Add character limit
+                // Check for shift and add character accordingly
+                if (Greenfoot.isKeyDown("shift")) {
+                    key = key.toUpperCase();
+                }
+                userName += key;
+            }
+            input.setValue(userName);
+        }
+    }
+
+    public void goMenuScreen() {
+        Greenfoot.setWorld(menuScreen);
+    }
 }
